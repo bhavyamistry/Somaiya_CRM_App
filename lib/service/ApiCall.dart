a6afcc684559080622156f4a734ae356de774ab8
Bhavya Mistry <bhavya.mistry@somaiya.edu>
2020-06-19 12:06:28 +0530

    bhavya-commit

--- lib/service/ApiCall.dart
+++ lib/service/ApiCall.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 import 'package:http/http.dart';
 import 'dart:convert';
-// String url = "http://kjsomaiya.epizy.com/";
-String url = "http://localhost:8080/somaiya/";
+import 'package:dio/dio.dart';    
+String url = "http://kjsomaiya.epizy.com/";
+// String url = "http://localhost:8080/somaiya/";
 class UserLogin
 {
   Map data,success;
@@ -18,44 +19,55 @@ class UserLogin
     };
     // String j_son = "{'user_id':$email, 'password':$passwd}";
     // Response response = await get(full_url);
-    try
-    {var response = await post(
-      full_url, 
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: jsonEncode({"user_id":email,"password":passwd}),
-      encoding: Encoding.getByName("utf-8")
-      );
-    if(response.body.isNotEmpty) {
-      print(response.body.toString());
-      Map result =json.decode(response.body);
-      success = result['success'];
-      print(success);
+    // try
+    // {
+      // var response = await post(
+      // full_url, 
+      // headers: {
+      //   "Content-Type": "application/json"
+      // },
+      // body: {"user_id":email,"password":passwd},
+      // encoding: Encoding.getByName("UTF-8")
+      // );
+      Dio dio = Dio();
+      try {
+        FormData formData = new FormData.fromMap({
+          "user_id": email,
+          "password": passwd,
+        });
+        var response = await dio.post(full_url, data: formData);
+        print(response.data);
+      } catch (e) {
+        print(e);
       }
-    }
-    catch(Exception)
-    {
-      print(Exception);
-    }
+    // if(response.body.isNotEmpty) {
+    //   print(response.body.toString());
+    //   Map result =json.decode(response.body);
+    //   success = result['success'];
+    //   print(success);
+    //   }
+    // }
+    // catch(Exception)
+    // {
+    //   print("Exception:$Exception");
+    // }
     // data = result['data'];
   }
 }
-class GoogleUserLogin
-{
-  Map data,success;
-  List user_data;
-  Future<List>  getCredentialsFromEmail(String email,String mode) async{
-    print("Fetching User Data");
-    String full_url = url + 'google_user_login.php?mode=$mode';
-    Map<String, String> headers = {"Content-type": "application/json"};
-    String j_son = "{'email_id':$email}";
-    // Response response = await get(full_url);
-    final Response response = await post(
-      full_url, headers: headers, body: j_son);
-    print(response.body.toString());
-    Map result = jsonDecode(response.body.toString());
-    success = result['success'];
-    // data = result['data'];
-  }
-}
+// class GoogleUserLogin
+// {
+//   Map data,success;
+//   List user_data;
+//   Future<List>  getCredentialsFromEmail(String email,String mode) async{
+//     print("Fetching User Data");
+//     String full_url = url + 'google_user_login.php?mode=$mode';
+//     Map<String, String> headers = {"Content-type": "application/json"};
+//     String j_son = "{'email_id':$email}";
+//     // Response response = await get(full_url);
+//     // final Response response = await post(
+//     //   full_url, headers: headers, body: j_son);
+//     // print(response.body.toString());
+//     // Map result = jsonDecode(response.body.toString());
+//     // success = result['success'];
+//     // data = result['data'];
+//   }
